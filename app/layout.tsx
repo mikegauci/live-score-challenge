56168527d5deaa76970467c066130a6591a4f273
mikegauci <mikegauci@gmail.com>
2023-09-10 18:35:35 +0200

    Update metas

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from "next"
 import StyledComponentsRegistry from "./registry"
 
 export const metadata: Metadata = {
-  title: "Live Score Website",
-  description: "Live Score Website",
+  title: "Live Score Challenge",
+  description: "Live Score Challenge",
 }
 
 export default function RootLayout({
